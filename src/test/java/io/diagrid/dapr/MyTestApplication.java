b99245976b0e47e44740811d521c1df455276c67
salaboy <Salaboy@gmail.com>
2023-10-19 09:25:52 +0100

    adding logs and test app

---
+++ src/test/java/io/diagrid/dapr/MyTestApplication.java
@@ -0,0 +1,11 @@
+package io.diagrid.dapr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyTestApplication.class, args);
+    }
+}
